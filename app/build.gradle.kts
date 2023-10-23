b20f574ee306c80164e84b17559a36321bc9c9bc
tjyoo73 <tjyoo73@naver.com>
2023-10-23 11:36:56 +0900

    2nd Commit - Glide - Internet Permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.github.chrisbanes:PhotoView:2.3.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/tjlab/librarypractice_genie/MainActivity.kt
+++ app/src/main/java/com/tjlab/librarypractice_genie/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tjlab.librarypractice_genie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.tjlab.librarypractice_genie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +20,7 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, ViewProfilePhotoActivity::class.java)
             startActivity(myIntent)
         }
+
+        Glide.with(this).load("https://s3.orbi.kr/data/file/united2/7559d4b48ce54f7dba55d2006a53851d.png").into(binding.lectureImg1);
     }
 }
